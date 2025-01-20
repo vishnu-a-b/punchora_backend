8b85b7b33942b44c9aa21f217eb7b1699e114a9b
arkkan <arkkanofficial@gmail.com>
2025-01-20 22:10:59 +0530

    router fix

--- src/modules/staff/routes/StaffRouter.ts
+++ src/modules/staff/routes/StaffRouter.ts
@@ -27,7 +27,7 @@ router.get(
   controller.get
 );
 router.get("/count-documents", staffCountDoc, controller.countTotalDocuments);
-router.get("/user/id", staffDetailsDoc, controller.getWithUserId);
+router.get("/user/:id", staffDetailsDoc, controller.getWithUserId);
 router.get("/:id", staffDetailsDoc, controller.getOne);
 router.post(
   "/",
