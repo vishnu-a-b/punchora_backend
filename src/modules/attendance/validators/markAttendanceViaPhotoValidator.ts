81eb8762abe77961d92e28eae96812c2c391eed3
arkkan <arkkanofficial@gmail.com>
2025-04-21 01:12:04 +0530

    fix

--- src/modules/attendance/validators/markAttendanceViaPhotoValidator.ts
+++ src/modules/attendance/validators/markAttendanceViaPhotoValidator.ts
@@ -1,7 +1,6 @@
 import { body } from "express-validator";
 
 export const markAttendanceViaPhotoValidator = [
-  body("time").isISO8601(),
   body("location").optional(),
   body("location.latitude").optional().isNumeric(),
   body("location.longitude").optional().isNumeric(),
