420207c535da38ce2769fb46002bc850e6f920dc
vishnu-a-b <vishnuab1207@gmail.com>
2025-11-20 11:34:09 +0530

    last location  fix

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'punchora-api',
+    script: './dist/src/server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    error_file: './logs/error.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true
+  }]
+};
